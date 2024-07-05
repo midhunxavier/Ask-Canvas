19e05f46465ded633f2194e143a10fbd4b105432
Midhun Xavier <midhunxavier@outlook.com>
2024-07-05 12:27:14 +0000

    removed files

--- Chatbot.py
+++ Chatbot.py
@@ -1,14 +1,26 @@
 from openai import OpenAI
 import streamlit as st
 
+import os
+from langchain.chains.openai_functions.openapi import get_openapi_chain
+from langchain.chains import APIChain
+from langchain.llms import OpenAI as LangchainOpenAI
+
+
 with st.sidebar:
+    canvas_access_token = st.text_input("Canvas Access Token", key="canvas_access_token", type="password")
+    "[Get a Canvas Access Token (LTU)](https://canvas.ltu.se/profile/settings)"
+    option = st.selectbox(
+    "Which university do you belong to?",
+    ("LTU", "KTH", "Chalmers", "Umeå"))
+    st.write("You selected:", option)
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[View the source code](https://github.com/midhunxavier/Ask-Canvas)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://laughing-waddle-5v596g6prpvc79wj.github.dev/)"
 
-st.title("💬 Ask Canvas")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.title("💬 Ask "+ option +" Canvas")
+st.caption("🚀 A Canvas chatbot powered by MX")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -20,10 +32,24 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
+
+    llm = LangchainOpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=openai_api_key)
+
+    headers = {"Authorization": f"Bearer {canvas_access_token}"}
+    chain = APIChain.from_llm_and_api_docs(
+        llm,
+        api_docs='https://canvas.ltu.se/doc/api/all_resources.html',
+        headers=headers,
+        verbose=False,
+        limit_to_domains=["https://canvas.ltu.se/api/v1/"],
+    )
+    response = chain.run(st.session_state.messages[-5:])
+    msg = response
+    #response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+
+    #msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
